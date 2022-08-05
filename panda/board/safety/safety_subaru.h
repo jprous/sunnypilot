9e0190d10d3d4e3562b764981bf7fa389fdb2378
JeanPierre Rousseau <108415873+jprous@users.noreply.github.com>
2022-08-05 20:45:03 +0200

    set / res button add
    This is to add "BO_ 326 Cruise_Buttons: 8 XXX"

--- panda/board/safety/safety_subaru.h
+++ panda/board/safety/safety_subaru.h
@@ -12,7 +12,7 @@ const int SUBARU_STANDSTILL_THRSLD = 20;  // about 1kph
 const int SUBARU_L_DRIVER_TORQUE_ALLOWANCE = 75;
 const int SUBARU_L_DRIVER_TORQUE_FACTOR = 10;
 
-const CanMsg SUBARU_TX_MSGS[] = {{0x122, 0, 8}, {0x221, 0, 8}, {0x322, 0, 8}};
+const CanMsg SUBARU_TX_MSGS[] = {{0x122, 0, 8}, {0x146, 0, 8}, {0x221, 0, 8}, {0x322, 0, 8}};
 #define SUBARU_TX_MSGS_LEN (sizeof(SUBARU_TX_MSGS) / sizeof(SUBARU_TX_MSGS[0]))
 
 AddrCheckStruct subaru_addr_checks[] = {
@@ -20,6 +20,7 @@ AddrCheckStruct subaru_addr_checks[] = {
   {.msg = {{0x119, 0, 8, .check_checksum = true, .max_counter = 15U, .expected_timestep = 20000U}, { 0 }, { 0 }}},
   {.msg = {{0x13a, 0, 8, .check_checksum = true, .max_counter = 15U, .expected_timestep = 20000U}, { 0 }, { 0 }}},
   {.msg = {{0x13c, 0, 8, .check_checksum = true, .max_counter = 15U, .expected_timestep = 20000U}, { 0 }, { 0 }}},
+  {.msg = {{0x146, 0, 8, .check_checksum = true, .max_counter = 15U, .expected_timestep = 20000U}, { 0 }, { 0 }}},
   {.msg = {{0x240, 0, 8, .check_checksum = true, .max_counter = 15U, .expected_timestep = 50000U}, { 0 }, { 0 }}},
 };
 #define SUBARU_ADDR_CHECK_LEN (sizeof(subaru_addr_checks) / sizeof(subaru_addr_checks[0]))
@@ -276,10 +277,11 @@ static int subaru_fwd_hook(int bus_num, CANPacket_t *to_fwd) {
   if (bus_num == 2) {
     // Global platform
     // 0x122 ES_LKAS
+    // 0x146 Cruise_Buttons
     // 0x221 ES_Distance
     // 0x322 ES_LKAS_State
     int addr = GET_ADDR(to_fwd);
-    int block_msg = ((addr == 0x122) || (addr == 0x221) || (addr == 0x322));
+    int block_msg = ((addr == 0x122) || (addr == 0x146) || (addr == 0x221) || (addr == 0x322));
     if (!block_msg) {
       bus_fwd = 0;  // Main CAN
     }
